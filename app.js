b85d461e69294976b0ee704111434544e287989b
hellojoelhuber <hello@joelhuber.com>
2022-10-03 12:32:09 -0500

    initial db config and setup, create users table
    Please see the README in the migrations folder for some instructions on how to setup the db.
    
    This creates the users table, sans profile icon.

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const authenticate = require('./src/auth.js')
 const bodyParser = require('body-parser')
+const { Pool, Client } = require('pg')
+const setup = require('./src/setup.js')
+
+// THIS IS ONE EXAMPLE OF HOW WE CAN CONNECT TO THE DB
+// IT WAS WRITTEN TO RUN THE MIGRATIONS, BEFORE I MOVED THAT
+// CODE TO setup.js. 
+// const pool = new Pool({
+// 	database: "cse_database",
+// 	user: "cse_username",
+// 	password: "cse_password",
+// 	host: "localhost",
+// 	port: 5432,
+// 	ensureDatabaseExists: true,
+// 	defaultDatabase: "postgres-cse"
+// })
+// const client = new Client(pool)
+// await client.connect()
+// try {
+	// setup.connect(pool, "./migrations")
+// } finally {
+	// client.end()
+// }
+
+setup.connect()
+
 
 const app = express()
 const jsonParser = bodyParser.json()
---
+++ src/setup.js
@@ -0,0 +1,23 @@
+const migrate = require("postgres-migrations");
+
+// exports.connect = async (pool, path) => {
+exports.connect = async () => {
+    const dbConfig = {
+        database: "cse_database",
+        user: "cse_username",
+        password: "cse_password",
+        host: "localhost",
+        port: 5432,
+
+        // Default: false for backwards-compatibility
+        // This might change!
+        ensureDatabaseExists: true,
+
+        // Default: "postgres"
+        // Used when checking/creating "database-name"
+        defaultDatabase: "postgres"
+    }
+
+    // await migrate.migrate(pool, path)
+    await migrate.migrate(dbConfig, "./migrations")
+}
