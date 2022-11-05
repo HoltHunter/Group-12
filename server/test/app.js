f122cfb8ada4716ac9d951994602984fc941524c
hellojoelhuber <hello@joelhuber.com>
2022-11-04 21:10:02 -0500

    test

--- server/test/app.js
+++ server/test/app.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 let app = require('../app')
-
+const before = require('chai').before
 const setup = require('../src/setup')
 
 
@@ -13,7 +13,9 @@ const baseUrl = 'http://localhost:8081'
 
 /* NOTE: You must be running the app server for these tests to pass. */
 
-setup.connect()
+before(async () => {
+    await setup.connect()
+})
 
 
 describe("Login (API)", function() {
