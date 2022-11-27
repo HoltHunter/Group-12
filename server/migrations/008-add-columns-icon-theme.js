3614ce687f592682f9982d3b61c9000bc22e6a85
hellojoelhuber <hello@joelhuber.com>
2022-11-27 15:36:01 -0600

    Added columns theme and icon
    default value for theme ‘classic’
    default value for icon ‘alpha’

---
+++ server/migrations/008-add-columns-icon-theme.js
@@ -0,0 +1,7 @@
+const addColumnIcon = require('./helpers/alter-users-add-icon')
+const addColumnTheme = require('./helpers/alter-users-add-theme')
+
+module.exports.generateSql = () => `
+    ${addColumnIcon}
+    ${addColumnTheme}
+`
