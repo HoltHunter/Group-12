8d4ec1ba8cc47ee0e7224a0d1145ccd727c74fb8
Michael Wienczkowski <mhw205@msstate.edu>
2022-11-30 19:57:27 -0500

    edit

--- server/test/app.js
+++ server/test/app.js
@@ -242,7 +242,7 @@ describe("Edit user properties", function() {
                 withCredentials: true,
                 headers: {"Content-Type": "application/json"}
             })
-            .send({ "settings": {"icon": "bravo"} })
+            .send({"icon": "bravo"})
             .end((err, res) => {
                 assert.equal(200, res.status)
                 done()
